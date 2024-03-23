c7fe6ead04a76877374ae637929c117dbf56c6db
youssefmetwalli <ymetwalli22@gmail.com>
2024-03-23 15:50:38 +0900

    edited the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { auth } from '../firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import Signin from './Signin'
+import Logout from './Logout'
 
 
 const NavBar = () => {
   const [user] = useAuthState(auth)
   return (
-    <div className='bg-gray-800 h-20 flex justify-between items-center p-4'>
+    <div className='bg-gray-800 h-20 w-full flex justify-between items-center p-4'>
         <h1 className='text-white text-3xl'> Chat App </h1>
-        <Signin />
+        {user ?  <Logout /> : <Signin />}
+        {/* <Signin />
+        <Logout /> */}
 
     </div>
   )
