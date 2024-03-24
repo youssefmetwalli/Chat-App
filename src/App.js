4ccc4c155b21417c7819854e2b0db0c2e87ef3c5
youssefmetwalli <ymetwalli22@gmail.com>
2024-03-24 22:03:19 +0900

    modifications

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Chat from './components/Chat'
 
 const style ={
   appContainer: 'max-w-[720px] mx-auto text-center',
-  sectionContainer: 'flex fslex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative'
+  sectionContainer: 'flex flex-col h-[90vh] bg-gray-100 mt-10 shadow-xl border relative'
 }
 
 function App() {
