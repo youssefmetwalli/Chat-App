7297fc6e69d0dd85c93de4967699f45966746de5
youssefmetwalli <ymetwalli22@gmail.com>
2024-04-01 23:25:29 +0900

    added the sendmessage feature

---
+++ src/components/SendMessage.jsx
@@ -0,0 +1,38 @@
+import { collection, serverTimestamp } from 'firebase/firestore';
+import React, { useState } from 'react';
+import { auth, db } from '../firebase';
+import { addDoc } from 'firebase/firestore';
+
+const SendMessage = () => {
+  const [input, setInput] = useState('');
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+    if (input === '') {
+        alert('Please enter a valid message')
+    }
+    const { uid, displayName } = auth.currentUser;
+    await addDoc(collection(db, 'messages'), {
+      text: input,
+      name: displayName,
+      uid,
+      timestamp: serverTimestamp()
+    });
+    setInput(''); 
+  };
+
+  return (
+    <form onSubmit={sendMessage} className='h-14 w-full max-w-[728px] flex text-xl absolute bottom-0 rounded-md '>
+      <input 
+        value={input} 
+        onChange={(e) => setInput(e.target.value)} 
+        type="text" 
+        placeholder='Message' 
+        className='w-full text-xl p-3 bg-gray-900 text-white outline-none border-none'/>
+
+      <button className='w-[20%] bg-green-400' type='submit'> Send </button>
+    </form>
+  );
+};
+
+export default SendMessage;
