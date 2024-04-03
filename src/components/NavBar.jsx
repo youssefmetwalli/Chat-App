6c1901f51691d1b9bfe0e6325d56f9846095e67e
youssefmetwalli <ymetwalli22@gmail.com>
2024-04-04 01:37:42 +0900

    enhancements

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,10 +9,8 @@ const NavBar = () => {
   const [user] = useAuthState(auth)
   return (
     <div className='bg-gray-800 h-20 w-full flex justify-between items-center p-4'>
-        <h1 className='text-white text-3xl'> Chat App </h1>
+        <h1 className='text-white text-3xl'> YoChat </h1>
         {user ?  <Logout /> : <Signin />}
-        {/* <Signin />
-        <Logout /> */}
 
     </div>
   )
