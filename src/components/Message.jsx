a4b5674b028762c35938b90216c071af0ff98dbe
youssefmetwalli <ymetwalli22@gmail.com>
2024-04-04 01:37:07 +0900

    appearance change based on user

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,18 +1,19 @@
-// Message.js
 import React from 'react'
+import { auth } from '../firebase'
+
+const style = {
+    sent: 'bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full',
+    received: 'bg-[#e5e5ea] text-black float-left rounded-br-full'
+}
 
 const Message = ({ message }) => {
-
-    const style = {
-        sent: 'bg-[#395dff] text-white flex-row-reverse text-end float-right rounded-bl-full',
-        received: 'bg-[#e5e5ea] text-black float-left rounded-br-full'
-    }
+    const messageStyle = 
+    message.uid === auth.currentUser.uid ? style.sent : style.received;
 
     return (
-        <div className={`flex items-center shadow-xl m-4 py-4 px-3 rounded-tl-full rounded-tr-full ${message.sent ? style.sent : style.received}`}>
-            <p className='text-gray-600 text-xs absolute mt-[-4.5rem] '>{message.name}  </p>
-            <p> {message.text} </p>
-            
+        <div className={`${messageStyle} flex items-center shadow-xl m-4 py-4 px-3 rounded-tl-full rounded-tr-full`}>
+            <p className='text-gray-600 text-xs absolute mt-[-4.5rem] '>{message.name}</p>
+            <p>{message.text}</p>
         </div>
     )
 }
